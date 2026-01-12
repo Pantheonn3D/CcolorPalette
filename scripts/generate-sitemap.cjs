59bad98404f0f1fe1fed20f347b551d8b82e5c20
Pantheonn3D <pantheon3d.contact@gmail.com>
2026-01-13 00:31:06 +0100

    changed urlset from w3 to sitemaps

--- scripts/generate-sitemap.cjs
+++ scripts/generate-sitemap.cjs
@@ -244,7 +244,7 @@ async function generateSitemap() {
   const today = new Date().toISOString().split('T')[0];
   
   let xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.w3.org/2000/sitemaps/schemas/sitemap/0.9"
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
   
   <!-- Core Pages -->
